e317c50581cb304341c58276112384f49190e4ce
DanielLSilv <130503794+DanielLSilv@users.noreply.github.com>
2024-08-14 20:49:36 -0300

    Create formdobro.js

---
+++ formdobro.js
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pagina01</title>
+    <script src="lib.js"></script>
+</head>
+<body>
+    <h1>Calculando dobro</h1>
+    <label for="valor">Valor Digitado</label>
+    <input type="number" id="valor" required><br><br>
+    <button onclick="mostrarResultado()"> Calcular Dobro</button>
+
+    <div id="resultado"></div>
+    
+</body>
+</html>
